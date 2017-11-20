9c67011b15d43e9ebfcfec6b12e903c3d316b28b
DanielJ <danielwjameson@yahoo.com>
2017-11-20 12:21:15 -0600

    Add CodeTranslator class

---
+++ include/CodeTranslator.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+class CodeTranslator {
+	public:
+		// CodeTranslator(file)
+		// string encode(string)
+		// string decode(string)
+
+	private:
+		// Tree encodeTree
+		// Tree decodeTree
+
+		// void buildEncodeTree(data)
+		// void buildDecodeTree()
+};
